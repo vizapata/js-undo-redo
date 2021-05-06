9b1ffabf51b43c4b49645836e0889cd111d6c257
Victor Zapata <victorzapatabedoya@hotmail.com>
2021-05-05 19:02:53 -0500

    Update npm tests
    Update npm tests to check the status of canUndo/canRedo flags

--- test.js
+++ test.js
@@ -6,28 +6,34 @@ const removeValue = (val) => counter -= val
 
 let tests = []
 undoRedoManager.execute(new Action(addValue, removeValue, 5))
-tests.push(counter === 5)
+tests.push(counter === 5 && status.canUndo && !status.canRedo)
 
 undoRedoManager.execute(new Action(addValue, removeValue, 3))
-tests.push(counter === 8)
+tests.push(counter === 8 && status.canUndo && !status.canRedo)
 
 undoRedoManager.execute(new Action(removeValue, addValue, 4))
-tests.push(counter === 4)
+tests.push(counter === 4 && status.canUndo && !status.canRedo)
 
 undoRedoManager.undo()
-tests.push(counter === 8)
+tests.push(counter === 8 && status.canUndo && status.canRedo)
 
 undoRedoManager.redo()
-tests.push(counter === 4)
+tests.push(counter === 4 && status.canUndo && !status.canRedo)
 
 undoRedoManager.undo()
-tests.push(counter === 8)
+tests.push(counter === 8 && status.canUndo && status.canRedo)
 
 undoRedoManager.undo()
-tests.push(counter === 5)
+tests.push(counter === 5 && status.canUndo && status.canRedo)
+
+undoRedoManager.execute(new Action(addValue, removeValue, 10))
+tests.push(counter === 15 && status.canUndo && !status.canRedo)
 
 undoRedoManager.undo()
-tests.push(counter === 0)
+tests.push(counter === 5 && status.canUndo && status.canRedo)
+
+undoRedoManager.undo()
+tests.push(counter === 0 && !status.canUndo && status.canRedo)
 
 if (tests.some(_ => !_)) throw new Error('Test failed')
 else console.log('All tests passed')
